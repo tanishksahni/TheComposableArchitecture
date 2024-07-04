b9cae80ef5e4e936bf002e82f61d139d86a62eaf
tanishksahni <tanishk.sahni2002@gmail.com>
2024-07-04 14:36:47 +0530

    Initial Commit

---
+++ TheComposableArchitecture/TheComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheComposableArchitectureApp.swift
+//  TheComposableArchitecture
+//
+//  Created by Tanishk Sahni on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TheComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
